ce199cf38dcd9c2ad942e328befcfaf5531e4796
payal vasave <udayvasave94@gmail.com>
2024-04-03 12:22:25 +0530

    test

--- src/Components/AllProjects.jsx
+++ src/Components/AllProjects.jsx
@@ -5,7 +5,7 @@ const AllProjects = () => {
   return (
     <>
     <div className="tempDiv">
-        <h1 className='h1TempDiv' style={{display:"flex",alignItems:'center',justifyContent:'center', height:'100vh'}}> We're currently working on improving this section! Stay tuned <span className='Emoji'>&#128522;</span></h1>
+        <h1 className='h1TempDiv' style={{display:"flex",alignItems:'center',justifyContent:'center', height:'100vh'}}> I'am currently working on improving this section! Stay tuned <span className='Emoji'>&#128522;</span></h1>
     </div>
     
     </>
