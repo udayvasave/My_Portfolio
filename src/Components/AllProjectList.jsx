931b774dd7311a419dd0bccfe6391cb8ba914bda
uday <udayvasave94@gmail.com>
2024-04-05 00:50:35 +0530

    added props in all project list

--- src/Components/AllProjectList.jsx
+++ src/Components/AllProjectList.jsx
@@ -5,45 +5,57 @@ import { FaGithub } from "react-icons/fa";
 import { FaGooglePlay } from "react-icons/fa";
 import { FaApple } from "react-icons/fa";
 
-const AllProjectList = ({title}) => {
+const AllProjectList = ({title, description, number, links}) => {
   return (
     <>
       <div className="snackoProject-allproject">
         <div className="numbering-projects">
-          <span style={{ fontSize: "2rem", fontWeight: "bold" }}>#1</span>
+          <span style={{ fontSize: "2rem", fontWeight: "bold" }}>{number}</span>
         </div>
         <div className="snacko1 list-container-allproject">
           <div className="proDescrp">
             <h2>{title}</h2>
             <span>
-              AI nutritional calorie tracker for 3.9 million products.
+             {description}
             </span>
           </div>
 
           <div className="linksAllpro">
-            <a href="https://play.google.com/store/apps/details?id=health.snacko">
+            {links.playstore && (
+                <a  href={links.playstore}>
               <span>
                 {" "}
                 <FaGooglePlay size={20} />{" "}
               </span>
             </a>
-            <a href="https://apps.apple.com/us/app/snacko-it/id6450678993">
-              <span>
-                {" "}
-                <FaApple size={20} />
-              </span>{" "}
-            </a>
-            {/* <a href="https://github.com/udayvasave">
-              {" "}
-              <span>
-                <FaGithub size={20} />{" "}
-              </span>{" "}
-            </a> */}
-            <a href="https://snacko.health/">
-              <span>
-                <IoLinkOutline size={20} />
-              </span>
-            </a>
+            )}
+            
+            {links.apple && (
+                <a href={links.apple}>
+                <span>
+                  {" "}
+                  <FaApple size={20} />
+                </span>{" "}
+              </a>
+            )}
+            
+            {links.github && (
+                 <a href={links.github}>
+                 {" "}
+                 <span>
+                   <FaGithub size={20} />{" "}
+                 </span>{" "}
+               </a>
+            )}
+           
+            {links.url && (
+                <a href={links.url}>
+                <span>
+                  <IoLinkOutline size={20} />
+                </span>
+              </a>
+            )}
+            
           </div>
         </div>
         </div>
--- src/Components/AllProjects.jsx
+++ src/Components/AllProjects.jsx
@@ -3,15 +3,74 @@ import "./allproject.css";
 import Navbar2 from "./Navbar2";
 import AllProjectList from "./AllProjectList";
 
-
 const AllProjects = () => {
+  const projectSnacko = 
+    {
+      number: "#1",
+      title: "Snacko",
+      description: "AI nutritional calorie tracker for 3.9 million products.",
+      linkss: {
+        github: "https://github.com/udayvasave",
+        url: "https://snacko.health/",
+        playstore:"https://play.google.com/store/apps/details?id=health.snacko",
+        apple:"https://apps.apple.com/us/app/snacko-it/id6450678993",
+      },
+    }
+  
+
+  const projectRoyalIndianVivah = 
+    {
+      number: "#2",
+      title: "Royal Indian Vivah",
+      description: "Matrimonial matchmaking web & mobile app",
+      linkss: {
+        github: "https://github.com/udayvasave",
+        url: "https://snacko.health/",
+        playstore:"https://play.google.com/store/apps/details?id=health.snacko",
+        // apple:"https://apps.apple.com/us/app/snacko-it/id6450678993",
+      },
+    }
+
+    const projectKanishkaConsultancy = 
+    {
+      number: "#3",
+      title: "Kanishka Consultancy",
+      description: "website for Insurance & Finance Consultant can handle leads in flutter app",
+      linkss: {
+        // github: "https://github.com/udayvasave",
+        url: "https://sidesambare.in",
+        // playstore:"https://play.google.com/store/apps/details?id=health.snacko",
+        // apple:"https://apps.apple.com/us/app/snacko-it/id6450678993",
+      },
+    }
+  
+
   return (
     <>
       <Navbar2 />
       <div className="All-projects">
-      <AllProjectList title='Snacko' />
-
-
+        <AllProjectList
+          title={projectSnacko.title}
+          description={projectSnacko.description}
+          number={projectSnacko.number}
+          links={projectSnacko.linkss}
+        />
+        <div className="sizedbox" style={{height:'1rem'}}></div>
+         
+         <AllProjectList
+          title={projectRoyalIndianVivah.title}
+          description={projectRoyalIndianVivah.description}
+          number={projectRoyalIndianVivah.number}
+          links={projectRoyalIndianVivah.linkss}
+        />
+         <div className="sizedbox" style={{height:'1rem'}}></div>
+         
+         <AllProjectList
+          title={projectKanishkaConsultancy.title}
+          description={projectKanishkaConsultancy.description}
+          number={projectKanishkaConsultancy.number}
+          links={projectKanishkaConsultancy.linkss}
+        />
       </div>
     </>
   );
